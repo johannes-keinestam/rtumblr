f533484c6800055494a7a1d937b73933b69c1213
Johannes Keinestam <johannes.keinestam@gmail.com>
2014-07-23 22:46:01 -0700

    Fixed to UI and cleanup of auth code.

--- init.py
+++ init.py
@@ -1,19 +1,18 @@
 import os
 import pytumblr
-from bottle import get, post, request, run, view, TEMPLATE_PATH, static_file, redirect
+from bottle import get, post, request, run, view, TEMPLATE_PATH, static_file, redirect, template
 import urlparse
 import oauth2 as oauth
 
 consumer_key = '04Liy0s8Gfxx7TyaojiPVN0JMMACuhM1TITjL8fw5j7X3vf0Pw'
 consumer_secret = '92QRq11NKjWy4Bo5jgF3edfu3QY3fUTFNCectAaEu7sFn1jLQC'
 
-
 class OAuthTumblrRestClient(pytumblr.TumblrRestClient):
 
     REQUEST_TOKEN_URL = 'http://www.tumblr.com/oauth/request_token'
     ACCESS_TOKEN_URL = 'http://www.tumblr.com/oauth/access_token'
     AUTHORIZE_URL = 'http://www.tumblr.com/oauth/authorize'
-    authenticated = False
+    user = None
 
     def authenticate(self, consumer_key, consumer_secret):
         if self.authenticated:
@@ -27,67 +26,80 @@ class OAuthTumblrRestClient(pytumblr.TumblrRestClient):
             raise Exception("Invalid response %s." % resp['status'])
         request_token = dict(urlparse.parse_qsl(content))
         self._oauth_token_secret = request_token['oauth_token_secret']
-        return "%s?oauth_token=%s" % (AUTHORIZE_URL, request_token['oauth_token'])
+        return "%s?oauth_token=%s" % (self.AUTHORIZE_URL, request_token['oauth_token'])
 
     def authenticate_step_2(self, oauth_verifier, oauth_token):
         token = oauth.Token(oauth_token, self._oauth_token_secret)
         token.set_verifier(oauth_verifier)
-        resp, content = oauth.Client(self._consumer, token).request(access_token_url, "POST")
+        resp, content = oauth.Client(self._consumer, token).request(self.ACCESS_TOKEN_URL, "POST")
         access_token = dict(urlparse.parse_qsl(content))
         # Authentication done, replace request object with authenticated one
         self._request = self.request
-        tumblr_client = pytumblr.TumblrRestClient(self.consumer_key, self.consumer_secret,
-                                                  access_token['oauth_token'],
-                                                  access_token['oauth_token_secret'])
-        self.authenticated = True
-        pprint(tumblr_client.info())
+        self.request = pytumblr.request.TumblrRequest(self.consumer_key, self.consumer_secret,
+                                                      access_token['oauth_token'],
+                                                      access_token['oauth_token_secret'])
+        self.user = self.info()['user']['name']
 
+    def logout(self):
+        assert self.authenticated, "You are not logged in -- cannot log out!"
+        self.request = self._request
+        self.user = None
 
-class KeyContainer():
-    def __init__(self): self.keys = {}
-    def get(self, key): return self.keys[key]
-    def set(self, key, val): self.keys[key] = val
+    @property
+    def authenticated(self):
+        return self.user != None
+
+def response_to_posts(post_response, sort=False):
+    posts = []
+    for post in post_response:
+        posts.append({
+            'blog':  post['blog_name'],
+            'type':  post['type'],
+            'notes': post['note_count'],
+            'link':  post['post_url']
+        })
+    if sort:
+        return sorted(posts, key=lambda p: int(p['notes']), reverse=sort=='up')
+    return posts
+
+def template_dict(**params):
+    result = {'pages': pages, 'username': tumblr_client.user}
+    result.update(params)
+    return result
 
 @get('/blog/<blog>')
 @view('templates/index2')
-def index(blog):
+def blog_view(blog):
     params = {}
     if request.query.ptype:
         params['type'] = request.query.ptype
     response = tumblr_client.posts(blog, **params)
-    posts = []
-    for raw_post in response['posts']:
-        posts.append({
-            'blog':  raw_post['blog_name'],
-            'type':  raw_post['type'],
-            'likes': 0,
-            'notes': raw_post['note_count'],
-            'link':  raw_post['post_url']
-        })
-    return {'posts': posts, 'blog_info': response['blog'], 'avatar_url': tumblr_client.avatar(blog)['avatar_url']}
+    posts = response_to_posts(response['posts'], request.query.sort)
+    return template_dict(posts=posts,
+                         title=response['blog']['name'],
+                         subtitle=response['blog']['title'],
+                         avatar_url=tumblr_client.avatar(blog)['avatar_url'])
+
+@get('/likes')
+@view('templates/index2')
+def likes_view():
+    import pprint
+    if tumblr_client.authenticated:
+        liked_posts = tumblr_client.likes()['liked_posts']
+        posts = response_to_posts(liked_posts, request.query.sort)
+    else:
+        posts = []
+    return template_dict(posts=posts, title='Likes')
 
 @get('/')
 @view('templates/index2')
-def index():
-    posts = [
-        {'blog': 'peacecorps', 'type': 'text', 'likes': 50, 'notes': 250, 'link': 'http://peacecorps.tumblr.com/post/92542423560/peace-corps-volunteers-support-new-let-girls'},
-        {'blog': 'peacecorps', 'type': 'text', 'likes': 50, 'notes': 250, 'link': 'http://peacecorps.tumblr.com/post/92542423560/peace-corps-volunteers-support-new-let-girls'},
-        {'blog': 'peacecorps', 'type': 'text', 'likes': 50, 'notes': 250, 'link': 'http://peacecorps.tumblr.com/post/92542423560/peace-corps-volunteers-support-new-let-girls'},
-        {'blog': 'peacecorps', 'type': 'text', 'likes': 50, 'notes': 250, 'link': 'http://peacecorps.tumblr.com/post/92542423560/peace-corps-volunteers-support-new-let-girls'},
-        {'blog': 'peacecorps', 'type': 'text', 'likes': 50, 'notes': 250, 'link': 'http://peacecorps.tumblr.com/post/92542423560/peace-corps-volunteers-support-new-let-girls'},
-        {'blog': 'peacecorps', 'type': 'text', 'likes': 50, 'notes': 250, 'link': 'http://peacecorps.tumblr.com/post/92542423560/peace-corps-volunteers-support-new-let-girls'},
-        {'blog': 'peacecorps', 'type': 'text', 'likes': 50, 'notes': 250, 'link': 'http://peacecorps.tumblr.com/post/92542423560/peace-corps-volunteers-support-new-let-girls'},
-        {'blog': 'peacecorps', 'type': 'text', 'likes': 50, 'notes': 250, 'link': 'http://peacecorps.tumblr.com/post/92542423560/peace-corps-volunteers-support-new-let-girls'},
-        {'blog': 'peacecorps', 'type': 'text', 'likes': 50, 'notes': 250, 'link': 'http://peacecorps.tumblr.com/post/92542423560/peace-corps-volunteers-support-new-let-girls'},
-        {'blog': 'peacecorps', 'type': 'text', 'likes': 50, 'notes': 250, 'link': 'http://peacecorps.tumblr.com/post/92542423560/peace-corps-volunteers-support-new-let-girls'},
-        {'blog': 'peacecorps', 'type': 'text', 'likes': 50, 'notes': 250, 'link': 'http://peacecorps.tumblr.com/post/92542423560/peace-corps-volunteers-support-new-let-girls'},
-        {'blog': 'peacecorps', 'type': 'text', 'likes': 50, 'notes': 250, 'link': 'http://peacecorps.tumblr.com/post/92542423560/peace-corps-volunteers-support-new-let-girls'},
-        {'blog': 'peacecorps', 'type': 'text', 'likes': 50, 'notes': 250, 'link': 'http://peacecorps.tumblr.com/post/92542423560/peace-corps-volunteers-support-new-let-girls'},
-        {'blog': 'peacecorps', 'type': 'text', 'likes': 50, 'notes': 250, 'link': 'http://peacecorps.tumblr.com/post/92542423560/peace-corps-volunteers-support-new-let-girls'},
-        {'blog': 'peacecorps', 'type': 'text', 'likes': 50, 'notes': 250, 'link': 'http://peacecorps.tumblr.com/post/92542423560/peace-corps-volunteers-support-new-let-girls'},
-        {'blog': 'peacecorps', 'type': 'text', 'likes': 50, 'notes': 250, 'link': 'http://peacecorps.tumblr.com/post/92542423560/peace-corps-volunteers-support-new-let-girls'}
-    ]
-    return {'posts': posts}
+def dashboard_view():
+    if tumblr_client.authenticated:
+        dashboard_posts = tumblr_client.dashboard()['posts']
+        posts = response_to_posts(dashboard_posts, request.query.sort)
+    else:
+        posts = []
+    return template_dict(posts=posts, title='Dashboard')
 
 @get('/static/<sfile:re:.+>')
 def get_static(sfile):
@@ -97,74 +109,27 @@ def get_static(sfile):
 
 @get('/login')
 def login():
-    consumer = oauth.Consumer(consumer_key, consumer_secret)
-    client = oauth.Client(consumer)
+    return redirect(tumblr_client.authenticate(consumer_key, consumer_secret))
 
-    # Step 1: Get a request token. This is a temporary token that is used for 
-    # having the user authorize an access token and to sign the request to obtain 
-    # said access token.
-
-    resp, content = client.request(request_token_url, "GET")
-    if resp['status'] != '200':
-        raise Exception("Invalid response %s." % resp['status'])
-
-    request_token = dict(urlparse.parse_qsl(content))
-
-    print "Request Token:"
-    print "    - oauth_token        = %s" % request_token['oauth_token']
-    print "    - oauth_token_secret = %s" % request_token['oauth_token_secret']
-    print 
-
-    keys.set('oauth_token_secret', request_token['oauth_token_secret'])
-    keys.set('consumer', consumer)
-    # Step 2: Redirect to the provider. Since this is a CLI script we do not 
-    # redirect. In a web application you would redirect the user to the URL
-    # below.
-
-    authorizi = "%s?oauth_token=%s" % (authorize_url, request_token['oauth_token'])
-    print "Go to the following link in your browser:"
-    print 
-    print authorizi
-    return redirect(authorizi)
-
-@post('/verify')
 @get('/verify')
-def login():
-    oauth_verifier = request.query.oauth_verifier
-    token = oauth.Token(request.query.oauth_token,
-                        keys.get('oauth_token_secret'))
-    token.set_verifier(oauth_verifier)
-    client = oauth.Client(keys.get('consumer'), token)
+def verify():
+    tumblr_client.authenticate_step_2(request.query.oauth_verifier, request.query.oauth_token)
+    return redirect('/')
 
-    resp, content = client.request(access_token_url, "POST")
-    access_token = dict(urlparse.parse_qsl(content))
-
-    print 'access token response: %s' % resp
-    print 'access token decoded: %s' % access_token
-    from pprint import pprint
-
-    print "Access Token:"
-    print "    - oauth_token        = %s" % access_token['oauth_token']
-    print "    - oauth_token_secret = %s" % access_token['oauth_token_secret']
-    print
-    print "You may now access protected resources using the access tokens above." 
-    print
-
-    tumblr_client = pytumblr.TumblrRestClient(consumer_key, consumer_secret,
-                                              access_token['oauth_token'],
-                                              access_token['oauth_token_secret'])
-    print 'you are:'
-    pprint(tumblr_client.info())
+@get('/logout')
+def logout():
+    tumblr_client.logout()
+    return redirect('/')
 
 @get('/hi')
 def say_hello():
-    return str(tumblr_client.info())
+    username = tumblr_client.info()['user']['name']
+    return '<p><h1>Welcome <b>%s</b></h1></p>' % username
 
-
-
-oauth_token_secret = None
-
-keys = KeyContainer()
+pages = {
+    'Dashboard': '/',
+    'Likes': '/likes',
+}
 TEMPLATE_PATH.append(os.path.dirname(os.path.realpath(__file__)))
-tumblr_client = pytumblr.TumblrRestClient(consumer_key)
+tumblr_client = OAuthTumblrRestClient(consumer_key)
 run(host='localhost', port=8080, reloader=True)
--- templates/index2.tpl
+++ templates/index2.tpl
@@ -29,10 +29,10 @@
 
   
     <!--[if lte IE 8]>
-        <link rel="stylesheet" href="static/css/layouts/blog-old-ie.css">
+        <link rel="stylesheet" href="/static/css/layouts/blog-old-ie.css">
     <![endif]-->
     <!--[if gt IE 8]><!-->
-        <link rel="stylesheet" href="static/css/layouts/blog.css">
+        <link rel="stylesheet" href="/static/css/layouts/blog.css">
     <!--<![endif]-->
   
 
@@ -48,8 +48,12 @@
 </head>
 <body>
 
-%setdefault('blog_info', {'name': 'ERROR', 'title': 'Choose a blog please!'})
+%setdefault('title', '')
+%setdefault('subtitle', '')
 %setdefault('avatar_url', 'http://assets.tumblr.com/images/favicons/favicon.ico')
+%setdefault('pages', {})
+%setdefault('username', '')
+%loggedin = bool(username)
 <div id="layout" class="pure-g">
     <div class="sidebar pure-u-1 pure-u-md-1-4">
         <div class="header">
@@ -61,11 +65,18 @@
             <nav class="nav">
                 <ul class="nav-list">
                     <li class="nav-item">
-                        <a class="pure-button" href="http://purecss.io">Blog</a>
+                    %if loggedin:
+                        <a class="pure-button" href="/logout">Logout {{username}}</a>
+                    %else:
+                        <a class="pure-button" href="/login">Login</a>
+                    %end
                     </li>
+                <br>
+                %for page, url in pages.iteritems():
                     <li class="nav-item">
-                        <a class="pure-button" href="http://yuilibrary.com">About</a>
+                        <a class="pure-button" href="{{url}}">{{page}}</a>
                     </li>
+                %end
                 </ul>
             </nav>
         </div>
@@ -75,14 +86,14 @@
         <div>
             <!-- A wrapper for all the blog posts -->
             <div class="posts">
-                <h1 class="content-subhead">Pinned Post</h1>
-
                 <!-- A single blog post -->
                 <section class="post">
                     <header class="post-header">
                         <img class="post-avatar" alt="Tilo Mitra&#x27;s avatar" height="48" width="48" src="{{avatar_url}}">
-                        <h2 class="post-title"><b>{{blog_info['name']}}:</b> <i>{{blog_info['title']}}</i></h2>
-
+                        <h2 class="post-title">{{title}}</h2>
+                        %if subtitle:
+                            <small>{{subtitle}}</small>
+                        %end
                     </header>
 
                     <div class="post-description">
@@ -93,103 +104,21 @@
                                     <th>Blog</th>
                                     <th>Link</th>
                                     <th>Type</th>
-                                    <th>Likes</th>
-                                    <th>Notes</th>
+                                    <th><a href="?sort=up">Notes</th>
                                 </tr>
                             </thead>
 
                             <tbody>
                             %for post in posts:
                                 <tr>
-                                    <th>{{post['blog']}}</th>
+                                    <th><a href="/blog/{{post['blog']}}">{{post['blog']}}</a></th>
                                     <th><a href="{{post['link']}}">{{post['link']}}</a></th>
                                     <th>{{post['type']}}</th>
-                                    <th>{{post['likes']}}</th>
                                     <th>{{post['notes']}}</th>
                                 </tr>
                             %end
                             </tbody>
                         </table>
-                        </p>
-                    </div>
-                </section>
-            </div>
-
-            <div class="posts">
-                <h1 class="content-subhead">Recent Posts</h1>
-
-                <section class="post">
-                    <header class="post-header">
-                        <img class="post-avatar" alt="Eric Ferraiuolo&#x27;s avatar" height="48" width="48" src="static/img/common/ericf-avatar.png">
-
-                        <h2 class="post-title">Everything You Need to Know About Grunt</h2>
-
-                        <p class="post-meta">
-                            By <a class="post-author" href="#">Eric Ferraiuolo</a> under <a class="post-category post-category-js" href="#">JavaScript</a>
-                        </p>
-                    </header>
-
-                    <div class="post-description">
-                        <p>
-                            Lorem ipsum dolor sit amet, consectetur adipisicing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur.
-                        </p>
-                    </div>
-                </section>
-
-                <section class="post">
-                    <header class="post-header">
-                        <img class="post-avatar" alt="Reid Burke&#x27;s avatar" height="48" width="48" src="static/img/common/reid-avatar.png">
-
-                        <h2 class="post-title">Photos from CSSConf and JSConf</h2>
-
-                        <p class="post-meta">
-                            By <a class="post-author" href="#">Reid Burke</a> under <a class="post-category" href="#">Uncategorized</a>
-                        </p>
-                    </header>
-
-                    <div class="post-description">
-                        <div class="post-images pure-g">
-                            <div class="pure-u-1 pure-u-md-1-2">
-                                <a href="http://www.flickr.com/photos/uberlife/8915936174/">
-                                    <img alt="Photo of someone working poolside at a resort"
-                                         class="pure-img-responsive"
-                                         src="http://farm8.staticflickr.com/7448/8915936174_8d54ec76c6.jpg">
-                                </a>
-
-                                <div class="post-image-meta">
-                                    <h3>CSSConf Photos</h3>
-                                </div>
-                            </div>
-
-                            <div class="pure-u-1 pure-u-md-1-2">
-                                <a href="http://www.flickr.com/photos/uberlife/8907351301/">
-                                    <img alt="Photo of the sunset on the beach"
-                                         class="pure-img-responsive"
-                                         src="http://farm8.staticflickr.com/7382/8907351301_bd7460cffb.jpg">
-                                </a>
-
-                                <div class="post-image-meta">
-                                    <h3>JSConf Photos</h3>
-                                </div>
-                            </div>
-                        </div>
-                    </div>
-                </section>
-
-                <section class="post">
-                    <header class="post-header">
-                        <img class="post-avatar" alt="Andrew Wooldridge&#x27;s avatar" height="48" width="48" src="static/img/common/andrew-avatar.png">
-
-                        <h2 class="post-title">YUI 3.10.2 Released</h2>
-
-                        <p class="post-meta">
-                            By <a class="post-author" href="#">Andrew Wooldridge</a> under <a class="post-category post-category-yui" href="#">YUI</a>
-                        </p>
-                    </header>
-
-                    <div class="post-description">
-                        <p>
-                            We are happy to announce the release of YUI 3.10.2! You can find it now on the Yahoo! CDN, download it directly, or pull it in via npm. We’ve also updated the YUI Library website with the latest documentation.
                         </p>
                     </div>
                 </section>
