dd8b495d979c8efe13798821b1a106f1ec68064b
Johannes Keinestam <johannes.keinestam@gmail.com>
2014-07-23 20:42:53 -0700

    Added class for Oauth rest client

--- init.py
+++ init.py
@@ -7,9 +7,40 @@ import oauth2 as oauth
 consumer_key = '04Liy0s8Gfxx7TyaojiPVN0JMMACuhM1TITjL8fw5j7X3vf0Pw'
 consumer_secret = '92QRq11NKjWy4Bo5jgF3edfu3QY3fUTFNCectAaEu7sFn1jLQC'
 
-request_token_url = 'http://www.tumblr.com/oauth/request_token'
-access_token_url = 'http://www.tumblr.com/oauth/access_token'
-authorize_url = 'http://www.tumblr.com/oauth/authorize'
+
+class OAuthTumblrRestClient(pytumblr.TumblrRestClient):
+
+    REQUEST_TOKEN_URL = 'http://www.tumblr.com/oauth/request_token'
+    ACCESS_TOKEN_URL = 'http://www.tumblr.com/oauth/access_token'
+    AUTHORIZE_URL = 'http://www.tumblr.com/oauth/authorize'
+    authenticated = False
+
+    def authenticate(self, consumer_key, consumer_secret):
+        if self.authenticated:
+            return
+        self.consumer_key = consumer_key
+        self.consumer_secret = consumer_secret
+        # Begin authentication
+        self._consumer = oauth.Consumer(consumer_key, consumer_secret)
+        resp, content = oauth.Client(self._consumer).request(self.REQUEST_TOKEN_URL, "GET")
+        if resp['status'] != '200':
+            raise Exception("Invalid response %s." % resp['status'])
+        request_token = dict(urlparse.parse_qsl(content))
+        self._oauth_token_secret = request_token['oauth_token_secret']
+        return "%s?oauth_token=%s" % (AUTHORIZE_URL, request_token['oauth_token'])
+
+    def authenticate_step_2(self, oauth_verifier, oauth_token):
+        token = oauth.Token(oauth_token, self._oauth_token_secret)
+        token.set_verifier(oauth_verifier)
+        resp, content = oauth.Client(self._consumer, token).request(access_token_url, "POST")
+        access_token = dict(urlparse.parse_qsl(content))
+        # Authentication done, replace request object with authenticated one
+        self._request = self.request
+        tumblr_client = pytumblr.TumblrRestClient(self.consumer_key, self.consumer_secret,
+                                                  access_token['oauth_token'],
+                                                  access_token['oauth_token_secret'])
+        self.authenticated = True
+        pprint(tumblr_client.info())
 
 
 class KeyContainer():
@@ -99,9 +130,8 @@ def login():
 @post('/verify')
 @get('/verify')
 def login():
-    print 'SECRET OAUTH TOKEN: %s' % keys.get('oauth_token_secret')
-    oauth_verifier = request.query.oauth_token
-    token = oauth.Token(request.query.oauth_verifier,
+    oauth_verifier = request.query.oauth_verifier
+    token = oauth.Token(request.query.oauth_token,
                         keys.get('oauth_token_secret'))
     token.set_verifier(oauth_verifier)
     client = oauth.Client(keys.get('consumer'), token)
@@ -109,12 +139,27 @@ def login():
     resp, content = client.request(access_token_url, "POST")
     access_token = dict(urlparse.parse_qsl(content))
 
+    print 'access token response: %s' % resp
+    print 'access token decoded: %s' % access_token
+    from pprint import pprint
+
     print "Access Token:"
-    print "    - oauth_token        = %s" % request.query.oauth_token
-    print "    - oauth_token_secret = %s" % keys.get('oauth_token_secret')
+    print "    - oauth_token        = %s" % access_token['oauth_token']
+    print "    - oauth_token_secret = %s" % access_token['oauth_token_secret']
     print
     print "You may now access protected resources using the access tokens above." 
     print
+
+    tumblr_client = pytumblr.TumblrRestClient(consumer_key, consumer_secret,
+                                              access_token['oauth_token'],
+                                              access_token['oauth_token_secret'])
+    print 'you are:'
+    pprint(tumblr_client.info())
+
+@get('/hi')
+def say_hello():
+    return str(tumblr_client.info())
+
 
 
 oauth_token_secret = None
